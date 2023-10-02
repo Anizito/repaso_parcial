0399fad2a971a6beb9bf45d839cc2ff1e803e539
Gerald <gpserrano.18.4@gmail.com>
2023-10-02 17:36:08 -0500

    feat: add weas

--- src/components/healthcheck-component.vue
+++ src/components/healthcheck-component.vue
@@ -1,15 +1,42 @@
 <script>
 import technogymtoolbarComponent from "@/components/technogymtoolbar-component.vue";
+import {TechnoApiService} from "@/services/techno-data";
 
 export default {
   name: "healthcheck",
   components: {technogymtoolbarComponent},
+  data(){
+    return{
+      products: [],
+      centers:[],
+      treadmills:[]
+    }
+  },
+  mounted(){
+    new TechnoApiService().getAllRecords().then((response) => {
+      this.products = response.data;
+    });
+
+    new TechnoApiService().getAllCenters().then((response) => {
+      this.centers = response.data;
+    });
+  }
 }
 </script>
 
 <template>
   <h1>HealthCheck view</h1>
-  <p>Card</p>
+  <pv-datatable :value="products" tableStyle="min-width: 50rem" paginator :rows="3" :rows-per-page-options="[3,6,9,13]">
+    <pv-column field= "id" header="Record Id"></pv-column>
+    <pv-column field="treadmillId" header="Treadmill Id"></pv-column>
+    <pv-column field="serialNumber" header="Serial Number"></pv-column>
+    <pv-column field="name" header="Center Name"></pv-column>
+    <pv-column field="quantity" header="Date"></pv-column>
+    <pv-column field="hour" header="Time"></pv-column>
+    <pv-column field="volts" header="Volts"></pv-column>
+    <pv-column field="watts" header="Watts"></pv-column>
+    <pv-column field="hp" header="HP"></pv-column>
+</pv-datatable>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import PrimeVue from "primevue/config";
 import Toolbar from "primevue/toolbar";
 import Button from "primevue/button";
 import Card from "primevue/card";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup'; 
+import Row from 'primevue/row'; 
 import my_router from "./router";
 
 const app = createApp(App);
@@ -18,5 +22,10 @@ app.use(my_router);
 app.component('pv-toolbar', Toolbar);
 app.component('pv-button', Button);
 app.component('pv-card', Card);
+app.component('pv-datatable',DataTable);
+app.component('pv-column', Column);
+app.component('pv-columngroup', ColumnGroup);
+app.component('pv-row', Row);
+
 
 app.mount("#app");
